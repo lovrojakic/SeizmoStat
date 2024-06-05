190728320e73b250521ddbd077e387ef0078b61e
Dino Gržanov <dino.grzanov@gmail.com>
2024-06-05 11:54:35 +0200

    + get frequency curl script

---
+++ Platform/curl_scripts/http_get_frequency.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -k -X GET -u intstv_seizmostat:N1mfSG25G4uUQIvp \
+https://djx.entlab.hr/m2m/data\?resourceSpec\=SiezmoStatFrequency \
+-H "Accept: application/vnd.ericsson.m2m.output+json;version=1.1"
